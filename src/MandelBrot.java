f156b83e15317427a25c26f1ec01e80f05a0875e
Soravaki <92048405+Soravaki@users.noreply.github.com>
2021-11-03 13:56:48 -0500

    School Work

--- src/MandelBrot.java
+++ src/MandelBrot.java
@@ -1,14 +1,54 @@
+import java.awt.Color;
 public class MandelBrot {
-    double x, y, a, b, imgWidth, imgHeight;
-    public MandelBrot(double x, double y, double a, double b, double imgWidth, double imgHeight){
-        this.x = x;
-        this.y = y;
-        this.a = a;
-        this.b = b;
-        this.imgWidth = imgWidth;
-        this.imgHeight = imgHeight;
+    double lowx,lowy,highx,highy;
+    Complex m;
+    Picture p;
+    int a, b;
+    public MandelBrot(double lowx, double lowy, double highx, double highy, int imgWidth, int imgHeight){
+        p = new Picture(imgWidth, imgHeight);
+        m = new Complex(0,0);
+        /*for (int a = 0; a<imgWidth; a++){
+            for (int b = 0; b<imgHeight; b++){
+                m = new Complex(a,b);
+                if (Sequence(m) ){
+                    p.set(a,b,Color.black);
+                }
+                else{
+                    p.set(a,b,Color.white);
+                }
+            }
+        }*/
+        m = new Complex(a,b);
+        for (double i=lowy; i<lowy; i += ( (lowy+lowy)/imgWidth) ){
+            for (double j=lowx; j<lowx; j += ( (lowx+lowx)/imgHeight) ){
+                a++;
+                if (Sequence(m) ){
+                    p.set(a,b,Color.black);
+                }
+                else{
+                    p.set(a,b,Color.white);
+                }
+                b++;
+            }
+        }
+        p.show();
+    }
 
-        Complex m = new Complex(1, 1);
+    public boolean Sequence(Complex val){
+        //System.out.println( m.add ( m.exp(2)) );
+        Complex sequence = val;
+        //System.out.println(sequence);
+        //System.out.println("start :: " + sequence);
+        for (int i=0; i<1000; i++){
+            sequence = val.add ( sequence.exp(2));
+            //System.out.println( sequence );
+            //System.out.println("sequence :: " + sequence);
+            if ( val.magnitude(sequence) > 2 ){
+                //System.out.println("magnitude :: "+ val.magnitude(sequence) + " :: " + sequence);
+                return false;
+            }
+        }
 
+        return true;
     }
 }
